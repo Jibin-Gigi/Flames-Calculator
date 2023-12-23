c4122cc0e6a505aaadbb396f84720a8d0f968a8a
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-12-24 02:18:33 +0530

    Flames Calculator

--- flamesCalculator.c
+++ flamesCalculator.c
@@ -10,12 +10,39 @@ int main() {
     scanf("%s", name1);
     printf("Enter your crush's name: ");
     scanf("%s", name2);
+    printf("\n");
 
     int length1 = strlen(name1);
     int length2 = strlen(name2);
 
     char result = flamesCalculation(name1, name2, length1, length2);
-    printf("Relationship status: %c\n", result);
+    
+    if(result=='f'){
+        printf("Relationship status: Friendship\n");
+        printf("“En Nanbane pol yaarum ille indha bhoomiyille” ");
+    }
+
+    if(result=='l'){
+        printf("Relationship status: Love\n");
+        printf("“Love is a dagger. It's a weapon to be wielded far away or up close. You can see yourself in it. It's beautiful.\n Until it makes you bleed. But ultimately when you reach for it... It isn't real. Love is an imaginary dagger.”");
+    }
+    if(result=='a'){
+        printf("Relationship status: Attraction\n");
+        printf("“What you seek is not seeking you.”");
+    }
+    if(result=='m'){
+        printf("Relationship status: Marriage\n");
+        printf("Congrats buddy \nSome random advice for you..Marriage is when two become one. The trouble starts when they try to decide which one.So better think twice ");
+            
+    }
+    if(result=='e'){
+        printf("Relationship status: Enemy\n");
+        printf("Oh, the misery.... Everybody wants to be my enemy....\n Spare the sympathy... Everybody wants to be my enemy-y-y-y-y (Look out for yourself)");
+    }
+    if(result=='s'){
+        printf("Relationship status: Siblings\n");
+        printf("In search of gold, you got a diamond");
+    }
 
     return 0;
 }
@@ -48,8 +75,6 @@ char flamesCalculation(char name1[], char name2[], int length1, int length2) {
     int max_size = 6, k = 0;
 
     int length = length1 + length2 - flag;
-
-    printf("%d\n", length);
 
     int i = 0;
     while (max_size > 1) {
